33acfc9443e3c1aa3211e560319c81f2b9c51507
Yisus Web <connexion.jesus@gmail.com>
2025-01-12 20:42:13 -0500

    Inicio curso

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -23,33 +23,36 @@
                 </div>
         </section>
 
-        <br>
-
-        <div class="grid" style="background: #25D366;">
-            <h1>
-                CIERRE CONTABLE TRIBUTARIO
-            </h1>
-        </div>
-
-        <div class="grid grid-cols-1 gap-4">
-            
-            <div class="grid grid-cols-6 gap-4 sm:gap-5 lg:gap-6">
-                <div class="card px-4 pb-4 sm:px-5">
-                    <div class="my-3 flex h-8 items-center justify-between">
-                        <h1 class="" style="font-size: 50px;">
-                            CIERRE CONTABLE TRIBUTARIO
-                        </h1>
-                    </div>
-                    <div class="max-w-2xl">
-                        <p>
-                        The Avatar component creates a scalable, colorable element that
-                        can have text, icon or image within its shape. Check out code
-                        for detail of usage.
-                        </p>
-                    </div>
+        <section style="padding: 40px;">
+            <div class="grid grid-cols-1 gap-4">
+                <div class="card" style="padding: 40px 20px;">
+                    <h1 style="font-size: 50px; text-align:center;">
+                        CURSO ESPECIALIZADO
+                    </h1>
                 </div>
             </div>
+        </section>
+        
+
+        <div class="grid grid-cols-2" style="padding: 80px 40px;">
+            <div style="padding: 0px 20px;">
+                <img src="{{ asset('themes/webpage/images/cc.jpeg') }}" alt="">
+            </div>
+            <div style="padding: 0px 20px; display: grid; place-items: center;">
+                <p style="font-size: 25px; line-height: 1.5;">
+                    Este curso está diseñado para capacitarte y
+                    actualizarte en los requerimientos establecidos en las
+                    Normas Internacionales de Información Financiera
+                    (NIIF) para un adecuado cierre de los estados
+                    financieros del año 2024. Asimismo, se abordará lo
+                    relacionado a la determinación del impuesto a la
+                    renta, considerando los criterios clave para la óptima
+                    deducción de los gastos, y de esa manera minimizar
+                    riesgos fiscales.
+                </p>
+            </div>
         </div>
+
     </main>
     
     
