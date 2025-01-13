cfb5471a8035aed7483c9ff2af5c240dae99a28c
Yisus Web <connexion.jesus@gmail.com>
2025-01-13 16:58:01 -0500

    modificaciones

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -23,37 +23,40 @@
                 </div>
         </section>
 
-        <section style="padding: 40px;">
-            <div class="grid grid-cols-1">
-                <div class="card" style="padding: 40px 20px;">
-                    <h1 style="font-size: 50px; text-align:center;">
+        <section style="padding: 20px 10px;">
+            <div class="mx-auto grid w-full grid-cols-1 gap-4 sm:grid-cols-1 sm:gap-5 lg:gap-6">
+                <div class="card" style="place-items: center; padding: 40px 0px;">
+                    <h1 style="font-size: 60px; line-height: 0.5; font-weight: 900; text-align:center; ">
                         CURSO TALLER
                     </h1>
                 </div>
             </div>
         </section>
-
-        <section style="padding: 80px 60px;">
-            <div class="grid grid-cols-2">
-                <div style="padding: 0px 20px;">
-                    <img src="{{ asset('themes/webpage/images/cc.jpeg') }}" alt="">
-                </div>
-                <div style="padding: 0px 20px; display: grid; place-items: center;">
-                    <p style="font-size: 25px; line-height: 1.5;">
-                        Este curso está diseñado para capacitarte y
-                        actualizarte en los requerimientos establecidos en las
-                        Normas Internacionales de Información Financiera
-                        (NIIF) para un adecuado cierre de los estados
-                        financieros del año 2024. Asimismo, se abordará lo
-                        relacionado a la determinación del impuesto a la
-                        renta, considerando los criterios clave para la óptima
-                        deducción de los gastos, y de esa manera minimizar
-                        riesgos fiscales.
-                    </p>
+        
+        <section style="padding: 20px;">
+            <div class="mx-auto mt-8 grid w-full grid-cols-1 gap-4 sm:grid-cols-1 sm:gap-5 lg:gap-6">
+                <div class="mx-auto mt-8 grid w-full grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-5 lg:gap-6">
+                    <div style="place-items: center;">
+                        <img src="{{ asset('themes/webpage/images/cc.jpg') }}" alt="">
+                    </div>
+                    <div class="p-4 sm:p-5" style="place-items: center;">
+                        <p style="font-size: 20px; line-height: 1.5;">
+                            Este curso está diseñado para capacitarte y
+                            actualizarte en los requerimientos establecidos en las
+                            Normas Internacionales de Información Financiera
+                            (NIIF) para un adecuado cierre de los estados
+                            financieros del año 2024. Asimismo, se abordará lo
+                            relacionado a la determinación del impuesto a la
+                            renta, considerando los criterios clave para la óptima
+                            deducción de los gastos, y de esa manera minimizar
+                            riesgos fiscales.
+                        </p>
+                    </div>
                 </div>
             </div>
         </section>
-        <section style="padding: 20px 60px;">
+
+        <section style="padding: 20px 40px;">
             <div class="mx-auto mt-8 grid w-full grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-5 lg:gap-6">
                 <div class="card p-4 sm:p-5" style="place-items: center;">
                     <img style="width: 80px;" src="{{ asset ('themes/webpage/images/icons/calendario.png') }}" alt="">
@@ -81,6 +84,32 @@
                     <p class="mt-1">
                         Virtual (online) con casuísticas reales.
                     </p>
+                </div>
+            </div>
+        </section>
+        
+        <section style="padding: 20px 40px;">
+            <div class="mx-auto mt-8 grid w-full grid-cols-1 gap-4 sm:grid-cols-1 sm:gap-5 lg:gap-6">
+                <div class="card p-4 sm:p-5" style="place-items: center;">
+                    <h2  style="font-size: 35px; line-height: 1.5; font-weight: 700;">
+                        ¿A QUIÉNES ESTÁ DIRIGIDO?
+                    </h2>
+                    <div class="mx-auto mt-8 grid w-full grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-5 lg:gap-6">
+                        <div class="p-4 sm:p-5" style="place-items: center;">
+                            <p style="font-size: 20px; line-height: 1.5;">
+                                Está dirigido a contadores, auditores, consultores
+                                contables y tributarios, así como a
+                                ejecutivos que toman decisiones
+                                financieras.
+                            </p>
+                        </div>
+                        <div class="p-4 sm:p-5" style="place-items: center;">
+                            <p style="font-size: 20px; line-height: 1.5;">
+                                También a quienes desean consolidar sus habilidades en la aplicación de normas contables y tributarias, asegurando un cierre 2024 eficiente, estratégico y libre de contingencias contables,
+                                financieras y fiscales.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
