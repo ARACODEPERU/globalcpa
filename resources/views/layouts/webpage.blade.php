ac5e73c64fc8cbd431b39de1d1512138d47890cb
Yisus Web <connexion.jesus@gmail.com>
2025-01-14 11:24:35 -0500

    probando containers

--- resources/views/layouts/webpage.blade.php
+++ resources/views/layouts/webpage.blade.php
@@ -12,14 +12,13 @@
 
     <title>CPA - Business School</title>
     <link rel="icon" type="image/png" href="themes/webpage/images/favicon.png" />
-{{--     
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-         --}}
+    
+    <link rel="stylesheet" href="themes/webpage/bootstrap-5.3.3/css/bootstrap.css" />
+    <link rel="stylesheet" href="themes/webpage/css/aracode.css" />
 
     <!-- CSS Assets -->
     <link rel="stylesheet" href="themes/webpage/css/app.css" />
-
-
+    
     <!-- Javascript Assets -->
     <script src="themes/webpage/js/app.js" defer></script>
 
