34811c2d656f0c4ccfde6c49139c0d7cb1efe438
José Carlos <60234854+Grandejosh@users.noreply.github.com>
2025-02-03 23:11:11 -0500

    d
    d

--- app/Http/Controllers/WebPageController.php
+++ app/Http/Controllers/WebPageController.php
@@ -174,7 +174,7 @@ class WebPageController extends Controller
             ->shuffle()
             ->take(3);
 
-        return view('pages.curso-descripcion', [
+        return view('pages.course-description', [
             'course' => $course,
             'item' => $item,
             'latest_courses' => $latest_courses
