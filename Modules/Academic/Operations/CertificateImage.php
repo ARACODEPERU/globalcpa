78a1bb17f11e4d24c1d150cab5668881ebae3f94
Grandejosh <grande_josh@odontologos.com>
2025-02-24 22:59:22 -0500

    d
    d

--- Modules/Academic/Operations/CertificateImage.php
+++ Modules/Academic/Operations/CertificateImage.php
@@ -190,7 +190,7 @@ class CertificateImage
                         $img->text("Lima, " . $fecha, $this->certificates_param->position_date_x, $this->certificates_param->position_date_y, function ($font) {
                             $font->file(public_path('fonts' . DIRECTORY_SEPARATOR . $this->certificates_param->fontfamily_date));
                             $font->size($this->certificates_param->font_size_date);
-                            $font->color('#0d0603');
+                            $font->color($this->certificates_param->color_date);
                             $font->align($this->certificates_param->font_align_date);
                             $font->valign($this->certificates_param->font_vertical_align_date);
                             $font->angle(0);
@@ -201,7 +201,7 @@ class CertificateImage
                         $img->text($student->person->full_name, $this->certificates_param->position_names_x, $this->certificates_param->position_names_y, function ($font) {
                             $font->file(public_path('fonts' . DIRECTORY_SEPARATOR . $this->certificates_param->fontfamily_names));
                             $font->size($this->certificates_param->font_size_names);
-                            $font->color('#0d0603');
+                            $font->color($this->certificates_param->color_names);
                             $font->align($this->certificates_param->font_align_names);
                             $font->valign($this->certificates_param->font_vertical_align_names);
                             $font->angle(0);
@@ -213,7 +213,7 @@ class CertificateImage
                         $img->text($this->wrapText($course->description, $max_width), $this->certificates_param->position_title_x, $this->certificates_param->position_title_y, function ($font) {
                             $font->file(public_path('fonts' . DIRECTORY_SEPARATOR . $this->certificates_param->fontfamily_title));
                             $font->size($this->certificates_param->font_size_title);
-                            $font->color('#0d0603');
+                            $font->color($this->certificates_param->color_title);
                             $font->align($this->certificates_param->font_align_title);
                             $font->valign($this->certificates_param->font_vertical_align_title);
                             $font->angle(0);
@@ -256,7 +256,7 @@ class CertificateImage
                             $img->text($line, $this->certificates_param->position_description_x, $currentY, function ($font) {
                                 $font->file(public_path('fonts' . DIRECTORY_SEPARATOR . $this->certificates_param->fontfamily_description));
                                 $font->size($this->certificates_param->font_size_description);
-                                $font->color('#0d0603');
+                                $font->color($this->certificates_param->color_description);
                                 $font->align($this->certificates_param->font_align_description);
                                 $font->valign($this->certificates_param->font_vertical_align_description);
                                 $font->angle(0);
