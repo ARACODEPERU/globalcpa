d175c4d16129b921b8369581be8188f387645ce3
Yisus Web <connexion.jesus@gmail.com>
2025-04-16 22:22:53 -0500

    suscripcion web

--- resources/views/pages/book-description.blade.php
+++ resources/views/pages/book-description.blade.php
@@ -480,10 +480,12 @@
         <br>
 
         <div class="mt-4 w-full transition-all duration-[.25s] sm:mt-5 lg:mt-6">
+            <br>
             <div style="text-align:center;">
                 <h1 class="title_aracode" style="font-size: 45px; line-height: 1.1; font-weight: 700;">
                     EDICIONES ANTERIORES
                 </h1>
+                <br>
             </div>
         </div>
 
