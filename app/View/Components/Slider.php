882198e77911ecea983cd77dd945769a766b7b37
Yisus Web <connexion.jesus@gmail.com>
2025-05-07 00:45:10 -0500

    slider cms

---
+++ app/View/Components/Slider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Modules\CMS\Entities\CmsSectionItem;
+
+class Slider extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    protected $sliders;
+
+    public function __construct()
+    {
+        $this->sliders = CmsSectionItem::with('item.items')->where('section_id', 4)->get();
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.slider', [
+            'sliders' => $this->sliders
+        ]);
+    }
+}
