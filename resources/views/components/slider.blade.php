cdb7ec48abd4523f3d645fa35bd6dc613631a470
Yisus Web <connexion.jesus@gmail.com>
2025-06-04 22:55:33 -0500

    conectado el slider

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -2,14 +2,14 @@
     <section class="bg-gray-100 flex items-center justify-center  w-full">
         <div class="slider w-full rounded-lg shadow-lg">
             <div class="slides">
-                {{-- @foreach ($sliders as $k => $slide)
+                @foreach ($sliders as $k => $slide)
                 <div class="slide">
                     <a href="{{ $slide->item->items[1]->content }}">
-                        <img src="{{ asset('storage/'.$slide->item->items[0]->content) }}" alt="Imagen 1" class="w-full">
+                        <img src="{{ asset('storage/'.$slide->item->items[0]->content) }}" alt="Imagen" class="w-full">
                     </a>
                 </div>
-                @endforeach --}}
-                <div class="slide">
+                @endforeach 
+                {{-- <div class="slide">
                     <img src="{{ asset('themes/webpage/images/slider_01.jpeg') }}" alt="Imagen 2" class="w-full">
                 </div>
                 <div class="slide">
@@ -23,7 +23,7 @@
                 </div>
                 <div class="slide">
                     <img src="{{ asset('themes/webpage/images/slider_05.jpeg') }}" alt="Imagen 3" class="w-full">
-                </div>
+                </div> --}}
             </div>
         </div>
     </section>
