80e9e115df43a62b91f0bdde519ebae4c9dc1efc
Yisus Web <connexion.jesus@gmail.com>
2025-06-04 22:26:20 -0500

    Precios visibles en la web

--- resources/views/pages/courses.blade.php
+++ resources/views/pages/courses.blade.php
@@ -77,7 +77,7 @@
                                             <button class="boton-degradado-courses">
                                                 <b>
                                                     <i class="fa fa-cart-plus" aria-hidden="true" style="font-size: 16px;"></i>
-                                                    &nbsp; Añadir al Carrito
+                                                    &nbsp; {{ $item->price }}
                                                 </b>
                                             </button>
                                         </a>
