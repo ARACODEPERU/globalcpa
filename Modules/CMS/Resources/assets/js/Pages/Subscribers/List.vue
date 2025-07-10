3e47b393344681b064fa5e6db39b6ee1cd73c2a3
José Carlos <grande_josh@outlook.com>
2025-07-09 20:01:12 -0500

    c
    c

--- Modules/CMS/Resources/assets/js/Pages/Subscribers/List.vue
+++ Modules/CMS/Resources/assets/js/Pages/Subscribers/List.vue
@@ -158,9 +158,12 @@
                                     <td class="border px-6 py-4">
                                         {{ subscriber.phone }}
                                     </td>
-                                    <!-- <td class="border px-6 py-4">
-                                         {{ subscriber.subject }}
-                                    </td> -->
+                                    <td class="border px-6 py-4 relative group">
+                                        <span class="cursor-help"><b>Curso/Tema</b></span>
+                                        <div class="absolute z-10 invisible group-hover:visible bg-gray-800 text-white text-xs rounded py-1 px-2 bottom-full left-1/2 transform -translate-x-1/2 mb-2 whitespace-nowrap">
+                                            {{ subscriber.subject }}
+                                        </div>
+                                    </td>
                                     <td class="border px-6 py-4">
                                         {{ subscriber.message }}
                                     </td>
