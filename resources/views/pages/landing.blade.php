aecf9daeb6742741be5d73916dbc02b5f0da4f24
Yisus Web <connexion.jesus@gmail.com>
2025-09-26 12:10:46 -0500

    k

--- resources/views/pages/landing.blade.php
+++ resources/views/pages/landing.blade.php
@@ -61,7 +61,6 @@
                         </div>
                     </div>
                 </div>
-                <br>
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-md-12">
