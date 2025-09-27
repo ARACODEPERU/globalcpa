cb2d70f72e49304adf0abf5a94af0ea4dc88a7eb
Yisus Web <connexion.jesus@gmail.com>
2025-09-27 14:42:19 -0500

    asdsad

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -8,34 +8,10 @@
                     href="{{ asset('themes/webpage/assets/svg/icon-sprite.svg#Grid') }}">
                 </use>
             </svg>
-            {{-- <form class="col-md-2 form-inline search-full" action="#" method="get">
-                <div class="form-group">
-                    <div class="Typeahead Typeahead--twitterUsers">
-                        <div class="u-posRelative">
-                            <input class="demo-input Typeahead-input form-control-plaintext w-100" type="text"
-                                placeholder="Type to Search .." name="q" title="" autofocus>
-                            <svg class="search-bg svg-color">
-                                <use href="{{ asset('themes/webpage/assets/svg/icon-sprite.svg#Search') }}"></use>
-                            </svg>
-                        </div>
-                        <div class="Typeahead-menu"></div>
-                    </div>
-                </div>
-            </form> --}}
 
             <img class="logo_header" src="{{ asset('themes/webpage/images/Logo_cpa_modificado.png') }}" alt="">
             <div class="nav-right col-auto pull-right right-header p-0 ms-auto">
                 <ul class="nav-menus">
-                    {{-- <li class="serchinput">
-                        <div class="serchbox">
-                            <svg>
-                                <use href="{{ asset('themes/webpage/assets/svg/icon-sprite.svg#Search') }}"></use>
-                            </svg>
-                        </div>
-                        <div class="form-group search-form">
-                            <input type="text" placeholder="Search here...">
-                        </div>
-                    </li> --}}
                     <li class="cart-nav onhover-dropdown">
                         <div class="cart-box" style="top: 8px;">
                             <a href="{{ route('web_carrito') }}">
@@ -46,36 +22,23 @@
                             </a>
                         </div>
                     </li>
-                    {{-- <li>
-                        <div class="mode">
-                            <i class="fa fa-moon" aria-hidden="true" style="font-size: 26px;"></i>
-                        </div>
-                    </li> --}}
-                    @auth
+                    {{-- @auth
                         <li class="profile-nav onhover-dropdown pe-0 py-0">
                             <div class="d-flex align-items-center profile-media">
-                                {{-- Muestra la imagen de perfil del usuario logueado --}}
 
                                 @php
-                                    // Obtenemos el nombre y el avatar del usuario
                                     $userName = Auth::user()->name;
                                     $userAvatar = Auth::user()->avatar;
-                                    // Verificamos si el archivo de la imagen existe en el disco 'public'
                                     $avatarExists = Storage::disk('public')->exists($userAvatar);
                                 @endphp
-
-                                {{-- Si el avatar existe, muestra la imagen --}}
                                 @if($avatarExists)
                                     <img class="b-r-25" src="{{ asset('storage' . $userAvatar) }}" alt="" style="max-height: 3rem; max-width: 3rem;">
                                 @else
-                                    {{-- Si el avatar no existe, muestra el círculo con las iniciales --}}
                                     <div class="initials-circle">
-                                        {{-- Obtenemos las iniciales del nombre --}}
                                         <span>{{ substr($userName, 0, 1) }}</span>
                                     </div>
                                 @endif
                                 <div class="flex-grow-1 user">
-                                    {{-- Muestra el nombre del usuario logueado --}}
                                     <span>{{ Auth::user()->name }}</span>
                                     <p class="mb-0 font-nunito">
                                         @if(Auth::user()->role === 'Alumno')
@@ -87,7 +50,7 @@
                                 </div>
                             </div>
                         </li>
-                    @endauth
+                    @endauth --}}
                     <li>
                         <a href="{{ route('login') }}" class="view-pc">
                             <button class="btn btn-pill btn-primary btn-air-primary btn-sm" type="button"
