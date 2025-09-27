f8ed25c8ecaa9f855c4a102867711885c673e01d
Yisus Web <connexion.jesus@gmail.com>
2025-09-27 01:41:18 -0500

    header responsive

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,4 @@
 <div>
-
-
-
     <div class="page-header">
         <div class="header-wrapper row">
             <div class="logo-wrapper"><a href=""><img class="img-fluid"
@@ -26,8 +23,7 @@
                 </div>
             </form> --}}
 
-            <img style="width: 280px;" src="{{ asset('themes/webpage/images/Logo_cpa_modificado.png') }}"
-                alt="">
+            <img class="logo_header" src="{{ asset('themes/webpage/images/Logo_cpa_modificado.png') }}" alt="">
             <div class="nav-right col-auto pull-right right-header p-0 ms-auto">
                 <ul class="nav-menus">
                     {{-- <li class="serchinput">
@@ -41,16 +37,13 @@
                         </div>
                     </li> --}}
                     <li class="cart-nav onhover-dropdown">
-                        <div class="cart-box" style="top: 12px;">
+                        <div class="cart-box" style="top: 8px;">
                             <a href="{{ route('web_carrito') }}">
                                 <i class="fa fa-cart-plus" aria-hidden="true" style="font-size: 26px;">
                                     <span class="cart-count contador" id="contadorCarritoWeb">0</span>
                                     <span id="contadorCarritoMovil" hidden
                                         style="color: white; display: none;"></span></i>
                             </a>
-                            <svg>
-                                {{-- <use href="themes/webpage/assets/svg/icon-sprite.svg#Buy"></use> --}}
-                            </svg>
                         </div>
                     </li>
                     {{-- <li>
@@ -96,11 +89,16 @@
                         </li>
                     @endauth
                     <li>
-                        <a href="{{ route('login') }}">
+                        <a href="{{ route('login') }}" class="view-pc">
                             <button class="btn btn-pill btn-primary btn-air-primary btn-sm" type="button"
                                 data-bs-original-title="btn btn-pill btn-primary btn-air-primary btn-sm">
-                                <i class="fa fa-cart-plus" aria-hidden="true" style="font-size: 18px;"></i>
                                 Campus Virtual
+                            </button>
+                        </a>
+                        <a href="{{ route('login') }}" class="view-movile">
+                            <button class="btn btn-pill btn-primary btn-air-primary btn-sm" type="button"
+                                data-bs-original-title="btn btn-pill btn-primary btn-air-primary btn-sm">
+                                Campus
                             </button>
                         </a>
                     </li>
@@ -108,60 +106,4 @@
             </div>
         </div>
     </div>
-
-
-    {{-- <header class="header">
-        <div class="header__container">
-            <img src="{{ asset('themes/webpage/slidebar/img/perfil.jpg') }}" alt="" class="header__img">
-            <a href="#" class="header__logo"></a>
-
-            <div class="header__search">
-                <input type="search" placeholder="Search" class="header__input">
-                <i class='bx bx-search header__icon'></i>
-            </div>
-
-            <div class="header__toggle">
-                <i class='bx bx-menu' id="header-toggle"></i>
-            </div>
-        </div>
-    </header> --}}
-
-
-    <style>
-        .cart-icon {
-            position: relative;
-            display: inline-block;
-        }
-
-        .cart-count {
-            position: absolute;
-            top: -7px;
-            left: 15px;
-            background-color: #6a4c93;
-            /* Color celeste */
-            color: #ffffff;
-            /* Color negro */
-            border-radius: 50%;
-            /* Para hacer un círculo */
-            width: 22px;
-            height: 22px;
-            line-height: 22px;
-            text-align: center;
-            font-size: 14px;
-            font-weight: 700;
-        }
-        .initials-circle {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 3rem;
-            height: 3rem;
-            border-radius: 50%; /* Esto crea la forma circular */
-            background-color: #007bff; /* Color de fondo, cámbialo a tu gusto */
-            color: white;
-            font-weight: bold;
-            font-size: 1.5rem; /* Ajusta el tamaño de la fuente */
-        }
-    </style>
-
 </div>
