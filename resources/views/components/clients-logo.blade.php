d3b538190e1c704291148539671982544d4ae37c
Yisus Web <connexion.jesus@gmail.com>
2025-10-30 18:20:44 -0500

    fgggg

--- resources/views/components/clients-logo.blade.php
+++ resources/views/components/clients-logo.blade.php
@@ -16,72 +16,78 @@
                 <div class="col-md-10">
                     <div class="row">
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/alicorp.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/alicorp.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/epsel.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/epsel.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/eps-grau.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/eps-grau.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/eps-maranon.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/eps-maranon.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
                             <img style="width: 100%;"
-                                src="{{ asset('themes/webpage/images/clients/electro-puno.jpg') }}" alt="">
+                                src="{{ asset('themes/webpage/images/clients/electro-puno.jpg') }}"  alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/redinter.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/redinter.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/interbank.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/interbank.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/bbva.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/bbva.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/pichincha.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/pichincha.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/bcp.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/bcp.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/horizonte.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/horizonte.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                         <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/antamina.jpg') }}"
-                                alt="">
+                            <img src="{{ asset('themes/webpage/images/clients/antamina.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
-                        <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/minsur.jpg') }}"
-                                alt="">
+                        <div class="col-md-2"
+                            style="margin-bottom: 20px;">
+                            <img src="{{ asset('themes/webpage/images/clients/minsur.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
-                        <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/entel.jpg') }}"
-                                alt="">
+                        <div class="col-md-2"
+                            style="margin-bottom: 20px;">
+                            <img src="{{ asset('themes/webpage/images/clients/entel.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
-                        <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/san-pablo.jpg') }}"
-                                alt="">
+                        <div class="col-md-2"
+                            style="margin-bottom: 20px;">
+                            <img src="{{ asset('themes/webpage/images/clients/san-pablo.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
-                        <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/finanty.jpg') }}"
-                                alt="">
+                        <div class="col-md-2"
+                            style="margin-bottom: 20px;">
+                            <img src="{{ asset('themes/webpage/images/clients/finanty.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
-                        <div class="col-md-2" style="margin-bottom: 20px;">
-                            <img style="width: 100%;" src="{{ asset('themes/webpage/images/clients/prima.jpg') }}"
-                                alt="">
+                        <div class="col-md-2"
+                            style="margin-bottom: 20px;">
+                            <img src="{{ asset('themes/webpage/images/clients/prima.jpg') }}" alt=""
+                                class="logo_clients">
                         </div>
                     </div>
                 </div>
@@ -89,4 +95,16 @@
             </div>
         </div>
     </section>
+
+    <style>
+        .logo_clients {
+            width: 100%;
+        }
+
+        @media (max-width: 480px) {
+            .logo_clients {
+                width: 220px;
+            }
+        }
+    </style>
 </div>
