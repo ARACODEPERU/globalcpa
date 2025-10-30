f51cd163d6797f020575bc5e6f7dfc26a5a3e268
Yisus Web <connexion.jesus@gmail.com>
2025-10-30 17:13:03 -0500

    sdsd

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -1,15 +1,18 @@
 <div>
-    <section class="bg-gray-100 flex items-center justify-center  w-full">
-        <div class="slider w-full rounded-lg shadow-lg">
-            <div class="slides">
-                @foreach ($sliders as $k => $slide)
-                <div class="slide">
-                    <a href="{{ $slide->item->items[1]->content }}">
-                        <img src="{{ asset('storage/'.$slide->item->items[0]->content) }}" alt="Imagen" class="w-full">
-                    </a>
+    <div class="container-fluid">
+        <section class="bg-gray-100 flex items-center justify-center  w-full">
+            <div class="slider w-full rounded-lg shadow-lg">
+                <div class="slides">
+                    @foreach ($sliders as $k => $slide)
+                        <div class="slide">
+                            <a href="{{ $slide->item->items[1]->content }}">
+                                <img src="{{ asset('storage/' . $slide->item->items[0]->content) }}" alt="Imagen"
+                                    class="w-full">
+                            </a>
+                        </div>
+                    @endforeach
                 </div>
-                @endforeach
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </div>
