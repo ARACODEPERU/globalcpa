95f3b01a4aee6ad24fd435fa8129958e5ead915a
Yisus Web <connexion.jesus@gmail.com>
2026-03-05 19:08:30 -0500

    kijk

--- resources/views/components/teachers.blade.php
+++ resources/views/components/teachers.blade.php
@@ -405,7 +405,7 @@
                                         aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body p-4 p-lg-5">
-                                    <h3 class="fw-bold text-gray-900 dark:text-[#fff] mb-1">{{ $teacher->item->items[0]->content }}</h3>
+                                    <h3 class="fw-bold text-gray-900 dark:!text-white mb-1">{{ $teacher->item->items[0]->content }}</h3>
                                     <p class="text-danger fw-bold text-uppercase mb-4">
                                         {{ $teacher->item->items[1]->content }}</p>
                                     <p class="text-gray-600 dark:text-gray-400" style="line-height: 1.8;">
