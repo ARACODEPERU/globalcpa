ad07b6074fb23856266d05b9edb534d8bb6bfe49
José Carlos <grande_josh@outlook.com>
2026-04-30 08:44:40 -0500

    s

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -167,7 +167,7 @@ class WebController extends Controller
     public function processPayment(Request $request, $id, $student_id)
     {
         $data = $request->input('cardFormData');
-        MercadoPagoConfig::setAccessToken(env('MERCADOPAGO_TOKEN'));
+        MercadoPagoConfig::setAccessToken(config('services.mercadopago.token'));
         $products = $data['products'];
         $data = (array)$data;
 
