e90e71337fd2f67028268914a0b8b192942ab335
José Carlos <grande_josh@outlook.com>
2026-05-05 18:23:26 -0500

    asd

--- resources/views/components/courselanding/investment.blade.php
+++ resources/views/components/courselanding/investment.blade.php
@@ -75,7 +75,7 @@
                                             </div>
 
                                             <div class="mb-4">
-                                                <del class="text-gray-600 dark:text-gray-400 fs-4 fw-semibold">S/ 
+                                                <del class="text-gray-600 dark:text-gray-400 fs-4 fw-semibold">S/
                                                     {{ $landing->investment_section['items'][0]['price_before'] }}</del>
                                                 <span class="text-gray-600 dark:text-gray-400 small">/
                                                     {{ $landing->investment_section['items'][0]['price_before_text'] ?? '' }}</span>
@@ -94,8 +94,11 @@
                                                 <i class="fa fa-shopping-cart me-2"></i> Inscribirse ahora
                                             </a>
                                             <button type="button" data-bs-toggle="modal" data-bs-target="#modalFinanciamiento"
-                                                class="btn btn-modern btn-modern-outline w-100 text-navy-custom">
-                                                <i class="fa fa-whatsapp me-2"></i> Asegurar vacante y/o financiamiento
+                                                    class="btn btn-modern btn-modern-outline w-100 text-navy-custom d-flex align-items-center justify-content-center gap-2">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20" height="20" style="flex-shrink: 0;">
+                                                    <path d="M380.9 97.1c-41.9-42-97.7-65.1-157-65.1-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480 117.7 449.1c32.4 17.7 68.9 27 106.1 27l.1 0c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3 18.6-68.1-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1s56.2 81.2 56.1 130.5c0 101.8-84.9 184.6-186.6 184.6zM325.1 300.5c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8s-14.3 18-17.6 21.8c-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7s-12.5-30.1-17.1-41.2c-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2s-9.7 1.4-14.8 6.9c-5.1 5.6-19.4 19-19.4 46.3s19.9 53.7 22.6 57.4c2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4s4.6-24.1 3.2-26.4c-1.3-2.5-5-3.9-10.5-6.6z"/>
+                                                </svg>
+                                                <span>Asegurar vacante y/o financiamiento</span>
                                             </button>
                                         </div>
                                     </div>
@@ -133,10 +136,13 @@
                                                 @endif
                                             </ul>
 
-                                            <a href="{{ $landing->corporate_contact_link ?? "#pageContactForm" }}" target="_blank"
-                                                class="btn btn-modern btn-modern-outline w-100 text-navy-custom">
-                                                <i class="fa fa-whatsapp me-2"></i> Contactar con un Asesor
-                                            </a>
+                                            <a href="{{ $landing->corporate_contact_link ?? '#pageContactForm' }}" target="_blank"
+                                                class="btn btn-modern btn-modern-outline w-100 text-navy-custom d-flex align-items-center justify-content-center gap-2">
+                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20" height="20" style="flex-shrink: 0;">
+                                                     <path d="M380.9 97.1c-41.9-42-97.7-65.1-157-65.1-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480 117.7 449.1c32.4 17.7 68.9 27 106.1 27l.1 0c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3 18.6-68.1-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1s56.2 81.2 56.1 130.5c0 101.8-84.9 184.6-186.6 184.6zM325.1 300.5c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8s-14.3 18-17.6 21.8c-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7s-12.5-30.1-17.1-41.2c-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2s-9.7 1.4-14.8 6.9c-5.1 5.6-19.4 19-19.4 46.3s19.9 53.7 22.6 57.4c2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4s4.6-24.1 3.2-26.4c-1.3-2.5-5-3.9-10.5-6.6z"/>
+                                                 </svg>
+                                                 <span>Contactar con un Asesor</span>
+                                             </a>
                                         </div>
                                     </div>
                                 </div>
@@ -158,6 +164,7 @@
                             </div>
                             <div class="modal-body p-4 p-lg-5 pt-0">
                                 <div class="text-center mb-4">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free v7.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M380.9 97.1c-41.9-42-97.7-65.1-157-65.1-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480 117.7 449.1c32.4 17.7 68.9 27 106.1 27l.1 0c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3 18.6-68.1-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1s56.2 81.2 56.1 130.5c0 101.8-84.9 184.6-186.6 184.6zM325.1 300.5c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8s-14.3 18-17.6 21.8c-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7s-12.5-30.1-17.1-41.2c-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2s-9.7 1.4-14.8 6.9c-5.1 5.6-19.4 19-19.4 46.3s19.9 53.7 22.6 57.4c2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4s4.6-24.1 3.2-26.4c-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
                                     <h2 class="fw-bold text-navy-custom text-[24px]">Asegurar vacante y/o financiamiento</h2>
                                     <p class="text-muted dark:text-gray-400">Déjanos tus datos para que un asesor te ayude con el proceso de inscripción y opciones de pago.</p>
                                 </div>
@@ -165,7 +172,7 @@
                                     @csrf
                                     <input type="hidden" name="subject" value="{{ $landing->course->description ?? 'Curso' }}">
                                     <input type="hidden" name="message" value="Desde Landing - Modal Financiamiento">
-                                    
+
                                     <div class="mb-3">
                                         <label class="form-label fw-bold text-navy-custom">Nombres y Apellidos</label>
                                         <div class="input-group shadow-sm">
