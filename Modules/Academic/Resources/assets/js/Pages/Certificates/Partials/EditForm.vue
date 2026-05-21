44616e78a722581c6020aabe346a712a79276925
José Carlos <grande_josh@outlook.com>
2026-05-21 12:45:33 -0500

    d

--- Modules/Academic/Resources/assets/js/Pages/Certificates/Partials/EditForm.vue
+++ Modules/Academic/Resources/assets/js/Pages/Certificates/Partials/EditForm.vue
@@ -248,6 +248,7 @@
 
         return null;
     });
+    const hasBackTemplateImage = computed(() => Boolean(form.back_certificate_img_preview || form.back_certificate_img));
 
     const activeBaseImage = computed(() => activeTab.value === 'back' ? backBaseImage.value : frontBaseImage.value);
     const canvasWidth = computed(() => {
@@ -2214,7 +2215,7 @@
                 </div>
                 <div class="h-px w-full border-b border-[#e0e6ed] dark:border-[#1b2e4b]"></div>
                 <div>
-                    <template v-if="activeTab === 'back' && !form.back_certificate_img_preview">
+                    <template v-if="activeTab === 'back' && !hasBackTemplateImage">
                         <div class="flex flex-col items-center justify-center h-64 bg-gray-100 dark:bg-gray-800 rounded-lg">
                             <svg class="w-16 h-16 text-gray-400 mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
