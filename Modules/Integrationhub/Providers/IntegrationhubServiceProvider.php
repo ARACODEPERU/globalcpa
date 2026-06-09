580c55a551416ba7b17224c134555dfae29c1e07
José Carlos <grande_josh@outlook.com>
2026-06-09 11:28:29 -0500

    a

--- Modules/Integrationhub/Providers/IntegrationhubServiceProvider.php
+++ Modules/Integrationhub/Providers/IntegrationhubServiceProvider.php
@@ -57,9 +57,9 @@ class IntegrationhubServiceProvider extends ServiceProvider
                      ->everyFifteenMinutes()
                      ->timezone('America/Lima');
 
-            // Ejecuta envío de WhatsApp de cumpleaños a las 7:00 AM
+            // Ejecuta envío de WhatsApp de cumpleaños a las 9:09 AM
             $schedule->command('integrationhub:birthday-whatsapp-send')
-                     ->dailyAt('07:00')
+                     ->dailyAt('09:09')
                      ->timezone('America/Lima');
         });
     }
