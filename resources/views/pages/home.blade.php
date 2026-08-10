9dae6e14cc58ec57967540084b3bd8f31723764b
Yisus Web <connexion.jesus@gmail.com>
2026-08-10 10:25:57 -0500

    vfgt

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -72,7 +72,7 @@
             <x-sidebar />
             <!-- Page Sidebar Ends-->
             <div class="page-body">
-                    <br><br>
+                    
                 <div data-aos="fade-in">
                      <x-slider />
                 </div>
