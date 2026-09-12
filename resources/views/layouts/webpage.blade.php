3c313b1d4dc7ad79dec1fcd0cee29ce26df389a7
José Carlos <grande_josh@outlook.com>
2026-09-12 01:20:25 -0500

    asd

--- resources/views/layouts/webpage.blade.php
+++ resources/views/layouts/webpage.blade.php
@@ -62,7 +62,20 @@
     <!-- latest jquery-->
     <script src="{{ asset('themes/webpage/assets/js/jquery.min.js') }}"></script>
 
+    <!-- Font Awesome 4 (ANTES de app.css) -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <link rel="stylesheet" href="{{ asset('themes/webpage/css/app.css') }}" />
+
+    <!-- Override FA6: forzar FA4 para .fa (DESPUES de app.css) -->
+    <style>
+        .fa,
+        .fa-classic,
+        .fa-serif {
+            font-family: FontAwesome !important;
+            font-weight: normal !important;
+        }
+    </style>
 
     <!-- Javascript Assets -->
     <!-- Carrito JS -->
@@ -75,10 +88,6 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
         rel="stylesheet" />
-
-
-    <!-- Font Awesome 4 (usado en testimonios, landing, etc.) -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <!-- Themify icon-->
     <link rel="stylesheet" type="text/css" href="{{ asset('themes/webpage/assets/css/vendors/themify.css') }}">
