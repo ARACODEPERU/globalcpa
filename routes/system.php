98722a98ff8a585d923c4b8dd9baf34417da5ce1
José Carlos <grande_josh@outlook.com>
2026-09-17 08:23:10 -0500

    rutas de billetera

--- routes/system.php
+++ routes/system.php
@@ -10,6 +10,10 @@ Route::middleware('auth')->group(function () {
         ->name('bank-account-store');
     Route::delete('bank/account/destroy/{id}', [BankAccountController::class, 'destroy'])
         ->name('bank-account-destroy');
+        Route::post('company/billetera/store', [CompanyBilleteraController::class, 'storeOrUpdate'])
+        ->name('company-billetera-store');
+    Route::delete('company/billetera/destroy/{id}', [CompanyBilleteraController::class, 'destroy'])
+        ->name('company-billetera-destroy');
     ///users///
     Route::middleware(['permission:usuarios'])
         ->get('datatables/users', [UserController::class, 'getUsers'])->name('users-tables-list');
